cc7dbb18b2bc77eb6ed7aeb32c58010c57db135d
Vasily <118783261+STALKRS228@users.noreply.github.com>
2022-12-30 02:03:28 +0200

    Add files via upload

---
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,21 @@
+﻿#include <windows.h>
+
+/* This is where all the input to the window goes to */
+class Sin {
+private:
+    int a, b;
+public:
+    auto sin() {
+        return b / a;
+    }
+};
+
+class Cos {
+    Sin s;
+private:
+    int a;
+protected:
+    auto cos() {
+        return a / s.sin();
+    }
+};
